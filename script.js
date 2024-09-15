61f3a6538f93377fbd58f79d260004458dfedda1
GreyArm <anklman7@me.com>
2024-09-15 10:45:03 +0100

    Open in a new window

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('bbcBtn').addEventListener('click', function() {
     console.log("Button clicked");
     const bbcLink = 'https://www.bbc.co.uk/';
-    window.open(bbcLink, '_blank');
+    window.open(bbcLink, '_blank', 'width=800,height=600');
     });
