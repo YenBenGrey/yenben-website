2636ac0d63602e197248d0b122eea5d1704d9c61
GreyArm <anklman7@me.com>
2024-09-15 10:36:31 +0100

    Console Log

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('bbcBtn').addEventListener('click', function() {
+    console.log("Button clicked");
     const bbcLink = 'https://www.bbc.co.uk/';
     window.open(bbcLink, '_blank');
     });
