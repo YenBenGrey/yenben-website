3933e2924d06ecda5ca9f00e91939f5107a8d74d
GreyArm <anklman7@me.com>
2024-09-15 10:15:41 +0100

    Discord link button

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('discordBtn').addEventListener('click', function() {
+    const discordLink = 'https://discord.gg/mDvpGDy9';
+    window.open(discordLink, '_blank');
+    });
