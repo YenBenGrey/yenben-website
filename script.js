d91025882f306a4227355e1e54d4d96d6fb62fa5
GreyArm <anklman7@me.com>
2025-01-12 13:40:53 +0000

    Starting from Scratch

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('bbcBtn').addEventListener('click', function() {
-    console.log("Button clicked");
-    const bbcLink = 'https://www.bbc.co.uk/';
-    window.open(bbcLink, '_blank', 'width=800,height=600');
-    });
